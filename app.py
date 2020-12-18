c91e00f839aa2418ecaa1ed37650b74588548194
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-12-18 11:32:28 -0600

    change Procfile

--- app.py
+++ app.py
@@ -299,12 +299,12 @@ def create_app(test_config=None):
     return app
 
 app = create_app()
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    
-# if __name__ == '__main__':
-#     app.run()
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    app.run()
 
 
         
