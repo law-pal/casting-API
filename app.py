ef20e51c107a55103234c5a83486eb8603f15f31
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-12-17 19:21:19 -0600

    heroku

--- app.py
+++ app.py
@@ -297,7 +297,11 @@ def create_app(test_config=None):
 
 
     return app
-    create_app().run(debug=True)
+    #create_app().run(debug=True)
+
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port, debug=True)
 
 # if __name__ == '__main__':
 #     APP.run(host='0.0.0.0', port=8080, debug=True)
