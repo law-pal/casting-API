51bbea788029ad27cc90050348c3adc318c81967
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2021-01-05 15:55:42 -0600

    issue with AUTH0

--- auth_config.py
+++ auth_config.py
@@ -2,6 +2,5 @@
 AUTH0_CLIENT_ID='lrRTRBboOlqLVLYxSCtmN3mT07oUtRuC'
 AUTH0_CLIENT_SECRET='pgcQuqIfd3FppUja8u2O92yNV4rTS6OAwoXn8RXFh-KiNEVgXR3-gKGk9CDcNROp'
 AUTH0_CALLBACK_URL='http://localhost:5000/callback'
-#AUTH0_CALLBACK_URL='http://localhost:5000/callback?code=pZXYlRN_k66StWvw&state=VUIqWn8Eok7XfCuPhydcDLWpm6QJ3g'
 AUTH0_DOMAIN='lawrencep.us.auth0.com'
 AUTH0_AUDIENCE='agency'
